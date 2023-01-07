9eee6aeb6375fb93a4aa36a5fc951c8ec8ba7f8d
Sergio Vinnicius de godoy <95051603+000-000-001@users.noreply.github.com>
2023-01-07 14:33:58 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const primerioNome = prompt("Informe seu primeiro nome recruta:")
+const sobrenome = prompt("Informe seu sobrenome nome recruta:")
+const campoDeEstudo = prompt("Informe sua área de estudo:")
+const dataDeNascimento = prompt ("Qual é o ano de nascimento do Recruta:")
+
+alert(    
+    "Recruta cadastrado com sucesso! \n" + ""+
+    "\nNome completo : " + primerioNome +""+ sobrenome +
+    "\nCampo de estudo: " + campoDeEstudo + 
+    "\nIdade" + (2022 - dataDeNascimento))
